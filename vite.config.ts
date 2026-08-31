ad184c3adfe81865206df579bc5d06036d06a878
BINTAMA <animasiasep887@gmail.com>
2026-08-31 22:44:45 +0700

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: './', // Menggunakan path relatif agar tidak blank saat di-deploy di GitHub Pages / Custom Domain
+});
